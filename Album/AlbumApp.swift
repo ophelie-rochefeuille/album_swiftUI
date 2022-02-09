7c37b0d0d60a9d2d1c09b87474515190fabefc5f
ophelie <rochefeuille.ophelie@gmail.com>
2022-02-09 11:40:23 +0100

    Initial Commit

---
+++ Album/AlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumApp.swift
+//  Album
+//
+//  Created by Ophélie Rochefeuille on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Album/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Album
+//
+//  Created by Ophélie Rochefeuille on 09/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
